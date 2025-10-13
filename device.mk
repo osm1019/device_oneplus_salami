34f14ad7fd5419c061e714de1bc8e7376730a06c
elpaablo <donopwhile1@gmail.com>
2025-10-13 21:25:19 +0100

    Disable anti-flicker and hbm mode for now
    Change-Id: Iac102ed32eea78e1a6618956aa682f6358d28845

--- device.mk
+++ device.mk
@@ -28,8 +28,15 @@ $(call soong_config_set_bool,qtidisplay,pxlw_hw_iris7,true)
 $(call soong_config_set,surfaceflinger,udfps_lib,//hardware/oplus:libudfps_extension.oplus)
 $(call soong_config_set_bool,qtidisplay,oplus_udfps,true)
 
+
 # Fingerprint
 TARGET_HAS_UDFPS := true
+
+# LiveDisplay
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_AF,false)
+$(call soong_config_set,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_SE,false)
+
+
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
