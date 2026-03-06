5ab0c1f0746b5fd3208748a8dc5e27bbd413f0e6
Oscar <osm1019@gmail.com>
2026-03-05 19:34:25 -0800

    salami: Initial Alphadroid bringup
    Change-Id: If51ad9b2ddc4aea7f729df78ab59bfc83622273b

--- alpha_salami.mk
+++ alpha_salami.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/salami/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/alpha/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_salami
+PRODUCT_NAME := alpha_salami
 PRODUCT_DEVICE := salami
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
